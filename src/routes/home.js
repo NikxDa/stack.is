12682fa13f9e787610e12cd75fad27da1be4abd9
NikxDa <NikxDa@users.noreply.github.com>
2019-01-04 23:09:59 +1100

    Update Mongoose options

--- src/routes/home.js
+++ src/routes/home.js
@@ -49,7 +49,7 @@ async function verifyUser (oAuthCode) {
     UserModel.findOneAndUpdate ({}, {
         user: userName, 
         accessToken
-    }, { upsert: true });
+    }, { upsert: true, new: true, setDefaultsOnInsert: true });
 
     // Return username
     return userName;
